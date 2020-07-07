cc3283966ce20d8d8afe8ff13d67a3f8c827304c
Spencer Broden <9060485+SpencerBroden@users.noreply.github.com>
2020-07-07 15:16:55 -0700

    fix color on first load

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
+setGradient()
+
 function setGradient() {
 	body.style.background = 
 	"linear-gradient(to right, " 
